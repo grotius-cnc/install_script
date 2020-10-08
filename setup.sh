bfdd61c4de2c1a4fb3ed6f6e0b34d33430fbd812
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2020-10-08 08:31:58 -0400

    Update setup.sh
    Needed to add super user su password

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 #This script is used to install ethercat on linux debian 10 buster.
 
-sudo apt install linux-headers-$(uname -r) #install missing debian buster kernel headers
-echo "Installing linux kernel header files for $(uname -r)"
-
-sudo apt-get install debhelper dkms gettext autoconf automake libtool quilt mercurial git xarchiver wget
-sudo apt-get install libexpat1-dev #needed by linuxcnc-ethercat
-echo "Installing some depenencies"
+echo "Installing linux kernel header files for $(uname -r) and dependencies"
+sudo apt-get install linux-headers-$(uname -r) debhelper dkms gettext autoconf automake libtool quilt mercurial git xarchiver wget libexpat1-dev dh-python rtai-modules-4.14.174-dbgsym tcl8.6-dev tk8.6-dev libreadline-gplv2-dev asciidoc dblatex docbook-xsl dvipng ghostscript graphviz groff imagemagick inkscape python-lxml source-highlight w3c-linkchecker xsltproc texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-lang-cyrillic texlive-lang-french texlive-lang-german texlive-lang-polish texlive-lang-spanish texlive-latex-recommended asciidoc-dblatex python-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev intltool libboost-python-dev netcat libmodbus-dev libusb-1.0-0-dev yapps2 libtirpc-dev
 
 # Download the first data..
 wget https://github.com/grotius-cnc/install_script/raw/main/ethercat
@@ -48,10 +44,6 @@ echo "Mac adres : $(cat /sys/class/net/enp0s25/address), writing the Mac adres t
 sudo cp ethercat /etc/default/
 echo "Copy ethercat file to system folder /etc/default/"
 
-# This permanent solution will set ethercat rules from root to user, dev/EtherCAT0 user permission's are ok at startup this way.
-sudo rm /etc/udev/rules.d/99-ethercat.rules
-sudo echo "KERNEL=="\"EtherCAT[0-9]*\"", MODE="\"0660\"", GROUP=""\"$USER\"" >> /etc/udev/rules.d/99-ethercat.rules
-
 # Update ethercat config
 sudo update-ethercat-config
 echo "Starting ethercat bus"
@@ -72,9 +64,6 @@ cd linuxcnc/debian/
 ./configure uspace
 cd ..
 
-# $ dpkg-checkbuilddeps
-sudo apt-get install dh-python rtai-modules-4.14.174-dbgsym tcl8.6-dev tk8.6-dev libreadline-gplv2-dev asciidoc dblatex docbook-xsl dvipng ghostscript graphviz groff imagemagick inkscape python-lxml source-highlight w3c-linkchecker xsltproc texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-lang-cyrillic texlive-lang-french texlive-lang-german texlive-lang-polish texlive-lang-spanish texlive-latex-recommended asciidoc-dblatex python-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev libgtk2.0-dev intltool libboost-python-dev netcat libmodbus-dev libusb-1.0-0-dev yapps2 libtirpc-dev
-
 cd src
 ./autogen.sh
 ./configure --with-realtime=uspace
@@ -89,6 +78,22 @@ cp lcec_conf ../../linuxcnc/bin/
 cd .. && cd .. && cd linuxcnc/scripts
 
 . ./rip-environment
+
+# This permanent solution will set ethercat rules from root to user, dev/EtherCAT0 user permission's are ok at startup this way.
+
+echo "Type your normal password to get a root account with the same password .."
+sudo passwd root
+
+echo "Login as root now, type password"
+su
+
+echo "Copy ethercat rules to etc/udev/rules.d/99-ethercat.rules/.."
+
+sudo rm /etc/udev/rules.d/99-ethercat.rules
+sudo echo "KERNEL=="\"EtherCAT[0-9]*\"", MODE="\"0660\"", GROUP=""\"$USER\"" >> /etc/udev/rules.d/99-ethercat.rules
+
+echo "Done, starting linuxcnc"
+
 linuxcnc # Integrate a ethercat example to the source.
 
 # old, for info :
